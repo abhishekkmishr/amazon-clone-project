197cb82653e3dc37abd1d1a699985c3a068e1525
Abhishek Mishra <132117431+abhishekkmishr@users.noreply.github.com>
2024-08-24 10:48:49 +0530

    Update api.jsx

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 // ================= Products Data Fetching Start here ===============
 export async function productsData() {
   const products = await axios.get(
-    "https://fakestoreapiserver.vercel.app/amazonproducts"
+    "https://fakestoreapi.vercel.app/products"
   );
   return products;
 }
